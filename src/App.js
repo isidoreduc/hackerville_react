52cb0ecdcb0993ad2b114d1a024a7a1c8bc2da18
isidoreduc <isidoreducasse79@gmail.com>
2019-03-07 17:22:07 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {list.map(item => 
+          <div key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+            <div>Author: {item.author}</div>
+            <div>Points: {item.points}</div>
+            <div>No. of comm: {item.num_comments}</div>
+            <hr/>
+          </div>
+          
+        )}
+
       </div>
+
     );
   }
 }
