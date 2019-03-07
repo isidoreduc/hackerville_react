c396622679083415ddea275d7ad786a1297d60dc
isidoreduc <isidoreducasse79@gmail.com>
2019-03-07 20:19:54 +0100

    a bit of searching

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import { LIST } from './model/model.js';
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      list: LIST
+      list: LIST,
+      searchTerm: ''
     }
     this.dismissItem = this.dismissItem.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
+
+  onSearchChange = (event) => this.setState({ searchTerm: event.target.value });
 
   dismissItem = (id) => {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({list: updatedList});
+    this.setState({ list: updatedList });
   }
+
 
   render() {
     return (
       <div className='container'>
-        {this.state.list.map(item => 
+        <form>
+          <div className='text-center'>
+            <input type='text' placeholder='search...' onChange={this.onSearchChange}></input>
+          </div>
+          <br />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div className='col text-center' key={item.objectID}>
             <a href={item.url}>{item.title}</a>
             <div>Author: {item.author}</div>
@@ -29,9 +41,9 @@ class App extends Component {
             <div>No. of comm: {item.num_comments}</div>
             <div><button onClick={() => this.dismissItem(item.objectID)} type='button'>
               Dismiss</button></div>
-            <hr className='col-4'/>
+            <hr className='col-4' />
           </div>
-          
+
         )}
 
       </div>
--- src/model/model.js
+++ src/model/model.js
@@ -1,18 +1,50 @@
 export const LIST = [
     {
-      title: 'React',
-      url: 'https://facebook.github.io/react/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
     },
     {
-      title: 'Redux',
-      url: 'https://github.com/reactjs/redux',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+    {
+        title: 'Angular',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 2
+    },
+    {
+        title: 'Vue',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 3,
+    },
+    {
+        title: 'Backbone',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 4
+    },
+    {
+        title: 'Ember',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 5,
     }
-  ]
+]
